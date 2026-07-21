dd22d9df3b3d423c0adf94afaf6c73cdebdeb21c
valpedro59 <valpedro59@gmail.com>
2026-07-21 23:10:15 +0100

    main program done

--- main.js
+++ main.js
@@ -1,8 +1,29 @@
-// variable
-
-let userWeight;
-let userHeight;
-
+// Function to calculate IMC
 const imcMaker = (userWeight, userHeight) => {
+  // Making the operation
   const imc = userWeight / (userHeight * userHeight);
+
+  //   conditional statement to determinate the category by imc
+  let category = "";
+  if (imc < 18.5) {
+    category = "maigreur";
+  } else if (imc >= 18.5 && imc < 24.9) {
+    category = "normal";
+  } else if (imc >= 25 && imc < 29.9) {
+    category = "surpoids";
+  } else if (imc >= 30) {
+    category = "obésité";
+  }
+  //   outpot the result
+  console.log(
+    `Votre IMC est : ${imc.toFixed(2)} . vous etes dans la categorie : ${category}`,
+  );
+  //   return the final value
+  return category;
 };
+
+// making some tests
+imcMaker(96, 1.7);
+imcMaker(66, 1.73);
+imcMaker(96, 1.8);
+imcMaker(50, 1.68);
